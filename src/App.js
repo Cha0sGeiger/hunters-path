69c78da3124d567043ff567a46536fd0b7e13973
Vjekoslav Jersic <vjekoslav.jersic@gmail.com>
2021-06-07 21:37:16 +0200

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import GameLogo from './assets/svg/hunters-path-logo.svg.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="game-logo" src={GameLogo} alt="hunters'path" />
     </div>
   );
 }
